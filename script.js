999e434b8d73ff9c7591763728fb22935b44ee06
muthuraman555 <88336757+muthuraman555@users.noreply.github.com>
2022-01-29 15:59:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function Validaion(){
+    let username = document.getElementById('username').value;
+    let mobile = document.getElementById('mobile').value;
+    let email = document.getElementById('email').value;
+
+    let usercheck = /^[A-Za-z ]{3,15}$/;
+    let emailcheck = /^[A-A-Za-z_]{3,}@[A-Za-z]{3,}[.]{1}[A-A-Za-z.]{2.6}$/;
+    let mobilecheck = /^[789][0-9]{9}$/;
+
+    if(usercheck.test(username)){
+        document.getElementById("usererror").innerHTML=" ";
+    }
+        else{
+            document.getElementById("usererror").innerHTML= "** User Name is Invalid";
+            return false;
+        }
+
+    if (emailcheck.test(email)){
+        document.getElementById("emailerror").innerHTML= "";
+    }
+    else{
+        document.getElementById("emailerror").innerHTML = "** Email is Invalid";
+        return false;
+    }
+
+    if (mobilecheck.test(mobile)){
+        document.getElementById("mobileerror").innerHTML= "";
+    }
+    else{
+        document.getElementById("mobileerror").innerHTML = "** Mobile is Invaild"
+
+        return false;
+    }
+}
+
